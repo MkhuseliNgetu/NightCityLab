f076d22e78ef171d58cb5bb9be43537407dcb91b
Mkhuseli Ngetu <105724548+MkhuseliNgetu@users.noreply.github.com>
2024-02-02 15:55:43 +0200

    Updated Loadshedding schedule and back-end calls

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -37,6 +37,7 @@
 
 export default {
   name: 'HeaderView',
+  
 }
 </script>
 <style scoped>
--- src/components/Status.vue
+++ src/components/Status.vue
@@ -1,13 +1,12 @@
 <template>
     <div class="status">
-        <div class="dashboarduptime">
+        <div class="dashboarduptime" v-if="!isHidden" v-on:click="isHidden = true" >
             <label>Loadshedding Schedule &ensp;</label>
-            <br/>
-            <label style="position: relative; right:8%">{{key}}</label>
-            <br/>
-            <vs-button icon color="danger" @click="GetDashboards()" style="position: relative; left:4%"><i class='bx bxs-calendar'></i>Update Loadshedding Schedule </vs-button>
         </div> 
-   
+        <div class="ac">
+            <label style="right:8%;white-space: pre-line">{{ key }}</label>
+            <vs-button icon color="danger" @click="GetDashboards()" v-if="!isHidden" v-on:click="isHidden = true" style="left:4%;"><i class='bx bxs-calendar'></i>Update Loadshedding Schedule </vs-button>
+        </div>
     </div>
 </template>
 
@@ -17,23 +16,17 @@ export default {
   name: 'StatusView',
   data() {
     return {
+      isHidden: false,
       Dashboards: null,
-      Cron: '5 * * * *',
-      key: "No LoadShedding",
-
-      CurrentStartTime: null,
-      CurrentEndTime: null,
-      CurrentStage: null,
-      UpcommingStartTime: null,
-      UpcommingEndTime: null,
-      UpcommingStage: null,
+      key: null,
     }
   },
   methods: {
     async GetDashboards() {
-      this.Dashboards = await fetch('https://nightcitybackend-e5lghbasoa-bq.a.run.app/Update').then(res => res.json())
+      this.Dashboards = await fetch('https://nigthcitylabbackend-e5lghbasoa-bq.a.run.app/Update').then(res => res.json())
       .then(data => this.key = data.schedule)
       .catch(err => console.log(err.schedule))
+      this.key = this.key.replace('/r','/')
     },
 }
 };
@@ -45,7 +38,6 @@ export default {
     position: fixed;
     height: auto;
 }
-
 label{
     font-size: 3rem;
     color: white;
